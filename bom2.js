b17d1d9cc9b5167a7c4b4da120e1b2e4d20eb336
atahirli19 <124487351+atahirli19@users.noreply.github.com>
2023-07-21 10:10:17 +0400

    test

---
+++ bom2.js
@@ -0,0 +1,58 @@
+//1
+
+// окно предупреждения, окно подтверждения и окно запроса
+alert(), confirm(), prompt()
+
+//2
+
+alert("Осторожно");
+
+//3
+
+window.confirm('Yes?') 
+
+//
+
+//4
+
+let answer = prompt("Как тебя зовут?", "");
+
+//5
+
+Time (function , milliseconds)
+
+//6
+
+setInterval()
+
+//7
+
+setTimeout(() => {
+    console.log('НАЧАЛО');
+  }, 0);
+
+  
+  //8
+
+  setInterval(() => console.log('ПОВТОР'), 2000);
+
+  //9
+
+  //можно сохранить любую информацию вы можете указать к какому пути принадлежит файл cookie.
+  // По умолчанию cookie принадлежит текущей странице.
+
+//10
+
+document.cookie = "Name=Ayla";
+
+//11
+
+document.cookie = "name=ayla; expires=Thu, 21 july 2023 12:00:00 UTC; path=/;";
+
+//12
+
+document.cookie = "name=; expires=Thu, 02 Jan 1979 00:00:00 UTC; path=/;";
+
+
+//zadaniya
+
